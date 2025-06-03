fec729d98db4b2a8f5e7ac21dc05a300aa2823de
Сергей Генерозов <cgenerozov@gmail.com>
2025-06-03 23:43:59 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: 'Фёдор Михайлович Достоевский',
+        name: 'Преступление и наказание',
+        publicationYear: 1866,
+        price: 500,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Лев Николаевич Толстой',
+        name: 'Война и мир',
+        publicationYear: 1869,
+        price: 650,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Александр Сергеевич Пушкин',
+        name: 'Евгений Онегин',
+        publicationYear: 1833,
+        price: 300,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Николай Васильевич Гоголь',
+        name: 'Мёртвые души',
+        publicationYear: 1842,
+        price: 400,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Антон Павлович Чехов',
+        name: 'Вишнёвый сад',
+        publicationYear: 1904,
+        price: 280,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Михаил Афанасьевич Булгаков',
+        name: 'Мастер и Маргарита',
+        publicationYear: 1967,
+        price: 520,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Александр Исаевич Солженицын',
+        name: 'Один день Ивана Денисовича',
+        publicationYear: 1962,
+        price: 430,
+        bestseller: false,
+        available: false
+    },
+    {
+        author: 'Борис Леонидович Пастернак',
+        name: 'Доктор Живаго',
+        publicationYear: 1957,
+        price: 480,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Илья Ильф и Евгений Петров',
+        name: 'Двенадцать стульев',
+        publicationYear: 1928,
+        price: 390,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Аркадий и Борис Стругацкие',
+        name: 'Трудно быть богом',
+        publicationYear: 1964,
+        price: 410,
+        bestseller: true,
+        available: false
+    }
+];
